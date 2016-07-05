b05270699150857eb46c8e3abb82313dd7abd194
emikod@mac.com <emikod@mac.com>
2016-07-05 13:53:49 -0600

    Add ajax

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,15 +1,19 @@
 class ItemsController < ApplicationController
   def create
     @user = User.find(params[:user_id])
-    @item = @user.items.build(item_params)
+    @item = @user.items.new(item_params)
+    @new_item = Item.new
 
     if @item.save
       flash[:notice] = "Item was saved successfully."
-      redirect_to[@user]
     else
       flash.now[:alert] = "There was an error saving the item. Please try again."
-      redirect_to[@user]
     end
+
+    respond_to do |format|
+       format.html
+       format.js
+     end
   end
 
   private
