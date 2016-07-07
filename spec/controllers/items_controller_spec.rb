6d3df02ca6a0f7cc2486c19718c22c1808f3cb46
emikod@mac.com <emikod@mac.com>
2016-07-07 13:26:13 -0600

    Pass delete spcs

--- spec/controllers/items_controller_spec.rb
+++ spec/controllers/items_controller_spec.rb
@@ -2,32 +2,36 @@ require 'rails_helper'
 
 RSpec.describe ItemsController, type: :controller do
 
-  let(:my_user) { create(:user) }
-  let(:my_item) { create(:item, user: my_user) }
-  let(:my_item2) { create(:item, user: my_user) }
+  let(:user) { create(:user) }
+  let(:item) { create(:item, user: user) }
+  let(:item2) { create(:item, user: user) }
 
   describe "POST create" do
-
     it "increases the number of Item by 1" do
-      expect{ post :create, format: :js, user_id: my_user.id, item: {name: my_item.name} }.to change(Item,:count).by(1)
+      expect{ post :create, user_id: user.id, item: {name: item.name} }.to change(Item,:count).by(1)
     end
 
     it "increases the sum of user items by 1" do
-      count = my_user.items.count
-      post :create, format: :js, user_id: my_user.id, item_id: my_item2.id
-      expect(my_user.items.count).to eq(count + 1)
+      count = user.items.count
+      post :create, user_id: user.id, item_id: item2.id
+      expect(user.items.count).to eq(count + 1)
     end
   end
 
   describe "DELETE destroy" do
+
+    before do
+      sign_in user
+    end
+
     it "deletes the item" do
-      delete :destroy, format: :js, user_id: my_user.id, id: my_item.id
-      count = Item.where({id: my_item.id}).count
+      delete :destroy, format: :js, user_id: user.id, id: item.id
+      count = Item.where({id: item.id}).count
       expect(count).to eq(0)
     end
 
     it "returns http success" do
-      delete :destroy, format: :js, user_id: my_user.id, id: my_item.id
+      delete :destroy, format: :js, user_id: user.id, id: item.id
       expect(response).to have_http_status(:success)
     end
   end
